eb0c59784c8d7d5165c1814bc39183111a8ca2b5
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2019-07-24 14:01:17 -0400

    Rewrite for better resilience

--- crypto.py
+++ crypto.py
@@ -7,81 +7,103 @@ import sublime_plugin
 class Crypto(sublime_plugin.ViewEventListener):
   def __init__(self, *args):
     super().__init__(*args)
-    self.password = None
+    self.enabled = False
+    self.encrypted = True
+    self.passwordCache = None
     self.fname = None
     self.busy = False
+    self.activating = False
 
-  # view gains focus, we use to detect opening and decrypt
   def on_activated(self):
-    fname = self.view.file_name()
-    if fname is None or os.path.splitext(fname)[1] != ".enc" or self.fname:
+    if not self.encrypted or self.activating:
       return
-    self.fname = fname
-    self.view.window().show_input_panel("Enter password", "", self.setup, None, self.close)
-
-  # encrypt before save
-  def on_pre_save(self):
     fname = self.view.file_name()
     if fname is None or os.path.splitext(fname)[1] != ".enc":
+      self.encrypted = False
       return
-    if fname != self.fname: # this was a save as / new file
-      self.fname = True
-      self.view.window().show_input_panel("Enter new password", "", self.setupEncrypt, None, self.setupEncrypt)
-      return
-    self.encrypt()
+    print("activated")
+    self.view.set_read_only(True)
+    self.activating = True
+    self.view.window().show_input_panel("Enter password", "", self.setup, None, self.disable)
 
-  # decrypt after save
+  def on_pre_save(self):
+    fname = self.view.file_name()
+    if fname != self.fname: # new file / save as
+      if fname is None or os.path.splitext(fname)[1] != ".enc":
+        return
+      print("saveas")
+      self.view.window().show_input_panel("Enter new password", "", self.setup, None, self.disable)
+    elif self.enabled and not self.encrypted:
+      print("pre")
+      self.encrypt()
+
   def on_post_save(self):
-    fname = self.view.file_name()
-    if fname is None or os.path.splitext(fname)[1] != ".enc" or not self.password:
+    if not self.enabled:
       return
-    if self.fname is True: # coming out of a new file / save as so dont decrypt
-      self.fname = fname
-      return
-    self.decrypt()
+    print("post")
+    if self.encrypted:
+      self.decrypt()
 
-  # reset scratch so dirty flag appears
   def on_modified(self):
-    fname = self.view.file_name()
-    if fname is None or os.path.splitext(fname)[1] != ".enc" or self.busy:
+    if not self.enabled or self.busy:
       return
     self.view.set_scratch(False)
 
   def setup(self, password):
-    self.password = password
-    self.decrypt()
-
-  def setupEncrypt(self, password=None):
-    if not password:
-      sublime.error_message("Password cannot be blank.")
+    self.passwordCache = password
+    self.fname = self.view.file_name()
+    self.enabled = True
+    self.view.set_read_only(False)
+    if self.encrypted:
+      self.activating = False
+      if not self.decrypt():
+        self.view.set_read_only(True)
+        self.disable()
+        self.activating = True
+        self.view.window().show_input_panel("Enter password", "", self.setup, None, self.disable)
     else:
-      self.password = password
-    self.view.run_command("save")
+      if self.passwordCache:
+        self.view.run_command("save") # encrypt
+        self.activating = False
+      else:
+        sublime.error_message("Invalid password.")
+        self.view.window().show_input_panel("Enter new password", "", self.setup, None, self.disable)
+
+  def disable(self):
+    print("disable")
+    self.enabled = False
+    self.fname = None
+    self.passwordCache = None
+    self.activating = False
 
   def encrypt(self):
-    self.busy = True # dont let on_modified reset dirty flag
-    self.view.run_command("sublime_utils_encrypt", {"password": self.password})
+    print("encrypting")
+    self.busy = True
+    self.view.run_command("sublime_utils_encrypt", {"password": self.passwordCache})
+    self.encrypted = True
 
   # decrypt, make it seem like file is unmodified
   def decrypt(self):
-    self.busy = True # dont let on_modified reset dirty flag
-    self.view.run_command("sublime_utils_decrypt", {"password": self.password})
+    print("decrypting")
+    self.busy = True
+    sublime.set_timeout_async(self.not_busy, 100)
+    self.view.run_command("sublime_utils_decrypt", {"password": self.passwordCache, "showErrors": False})
     if self.view.get_status("sublime_utils_decrypt_status") == "Failed":
-      self.view.close()
-      return
+      self.encrypted = True
+      sublime.error_message("Invalid password.")
+      return False
+    self.encrypted = False
     self.view.erase_status("sublime_utils_decrypt_status")
     self.view.reset_reference_document() # diff on left
     self.view.set_scratch(True) # no dirty flag in tab
-    sublime.set_timeout_async(self.not_busy, 100) # timeout to enable on_modified
-
-  def close(self):
-    self.view.close()
+    return True
 
   def not_busy(self):
     self.busy = False
 
+
 class SublimeUtilsEncryptCommand(sublime_plugin.TextCommand):
-  def run(self, edit, password):
+  def run(self, edit, password, showErrors=True):
     plaintext = self.view.substr(sublime.Region(0, self.view.size()))
     cmd = ["gpg", "-c", "--batch", "--passphrase", password]
     process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -93,12 +115,13 @@ class SublimeUtilsEncryptCommand(sublime_plugin.TextCommand):
       process.wait()
       raise
     if errs:
-      sublime.error_message("Encryption failed with code {}:\n{}".format(retcode, errs.decode()))
+      if showErrors:
+        sublime.error_message("Encryption failed with code {}:\n{}".format(retcode, errs.decode()))
       return
     self.view.replace(edit, sublime.Region(0, self.view.size()), base64.b64encode(ciphertext, b"+\n").decode())
 
 class SublimeUtilsDecryptCommand(sublime_plugin.TextCommand):
-  def run(self, edit, password):
+  def run(self, edit, password, showErrors=True):
     ciphertext = base64.b64decode(self.view.substr(sublime.Region(0, self.view.size())), b"+\n")
     cmd = ["gpg", "-d", "-q", "--batch", "--passphrase", password]
     process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -111,7 +134,8 @@ class SublimeUtilsDecryptCommand(sublime_plugin.TextCommand):
       raise
     if errs:
       self.view.set_status("sublime_utils_decrypt_status", "Failed")
-      sublime.error_message("Decryption failed with code {}:\n{}".format(retcode, errs.decode()))
+      if showErrors:
+        sublime.error_message("Decryption failed with code {}:\n{}".format(retcode, errs.decode()))
       return
     self.view.set_status("sublime_utils_decrypt_status", "Success")
     self.view.replace(edit, sublime.Region(0, self.view.size()), plaintext.decode())
