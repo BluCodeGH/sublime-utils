64519279090ddb6edbccac09587862b4ba8ed4d7
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2020-05-05 20:03:21 -0400

    Rewrite open/save dialogue to use fzf and floating kitty window

--- files.py
+++ files.py
@@ -1,69 +1,31 @@
-import functools
 import os
 import subprocess
+import tempfile
 import sublime
 import sublime_plugin
 
 class SublimeFilesOpenCommand(sublime_plugin.WindowCommand):
-  def __init__(self, *args):
-    super().__init__(*args)
-    self.active_view = None # the original view we can use to hide transient previews
-    self.hidden = False # show hidden files?
-
   def run(self):
-    self.active_view = self.window.active_view()
-    self.hidden = False
-    self.navigate("~")
-
-  # generate a list of all the files in a given directory
-  def navigate(self, folder):
-    self.window.show_quick_panel([], None) # clear any existing quick panels
-    folder = os.path.abspath(os.path.expanduser(folder))
-    data = ["[{}]".format(folder)] # title of the current directory
-    if not self.hidden:
-      data.append(".*") # option to show hidden files
-    if not os.path.samefile(folder, "/"):
-      data.append("..") # option to go one folder up
-    cmd = ["rg", "-u" if not self.hidden else "-uu", "--files", folder] # use ripgrep to list files
-    #cmd = ["find", folder, "-mindepth", "1", "-type", "f"]
-    process = subprocess.Popen(cmd, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    temp = os.path.join(tempfile.gettempdir(), next(tempfile._get_candidate_names()))
+    fifo = None
     try:
-      cut = len(folder)
-      if folder[-1] != "/": # don't cut the first letter of paths after /
-        cut += 1
-      data.extend([l[cut:] for l in process.communicate()[0].splitlines()])
-    except:
-      process.kill()
-      process.wait()
-      raise
-    # show quick panel, passing the folder and data to callbacks
-    self.window.show_quick_panel(data, functools.partial(self.chosen, folder, data), sublime.MONOSPACE_FONT, 1, functools.partial(self.preview, folder, data))
+      os.mkfifo(temp)
+      fifo = os.open(temp, os.O_RDONLY | os.O_NONBLOCK)
 
-  # the user presses enter on an item or cancels (i=-1)
-  def chosen(self, folder, data, i):
-    if i < 1:
-      self.window.focus_view(self.active_view) # hide the transient preview
-      return
-    if data[i] == ".*":
-      self.hidden = True
-      self.navigate(folder) # redisplay the current folder with hidden files shown
-      return
-    file = os.path.join(folder, data[i])
-    if data[i] == "..":
-      self.navigate(file)
-      return
-    self.window.open_file(file)
+      cmd = [
+        "kitty", "--class", "popup", "-d", "~", "bash", "-c",
+        "fzf --info=hidden --preview=\"less {} 2>/dev/null\" --preview-window=right:50% --bind \"enter:accept+execute(echo {} > " + temp + ")\""
+      ]
+      subprocess.Popen(cmd).wait()
 
-  # a new quick panel item is highlighted
-  def preview(self, folder, data, i):
-    if i < 1 or data[i] in ["..", ".*"]:
-      self.window.focus_view(self.active_view) # hide the transient preview
-      return
-    file = os.path.join(folder, data[i])
-    if os.path.getsize(file) < 1e6: # the file is small enough to load quickly
-      self.window.open_file(file, sublime.TRANSIENT) # show a transient (no tab) preview
-    else:
-      self.window.focus_view(self.active_view) # hide the transient preview
+      toOpen = os.read(fifo, 1024).decode()
+      if toOpen:
+        toOpen = os.path.join(os.path.expanduser("~"), toOpen[:-1])
+        self.window.open_file(toOpen)
+    finally:
+      if fifo:
+        os.close(fifo)
+      os.remove(temp)
 
 class SublimeFilesSaveCommand(sublime_plugin.TextCommand):
   def run(self, _):
@@ -73,58 +35,27 @@ class SublimeFilesSaveCommand(sublime_plugin.TextCommand):
       self.view.run_command("save")
 
 class SublimeFilesSaveAsCommand(sublime_plugin.TextCommand):
-  def __init__(self, *args):
-    super().__init__(*args)
-    self.preview = None # a view that will be used to list the contents of chosen directories
-
   def run(self, _):
-    blank = os.path.join(os.path.dirname(__file__))
-    self.preview = self.view.window().open_file(blank) # setup the preview view
-    self.preview.set_scratch(True) # dont mark as dirty
-    self.navigate("~")
-
-  # list all folders in a directory
-  def navigate(self, folder):
-    self.view.window().show_quick_panel([], None) # hide existing quick panel
-    folder = os.path.abspath(os.path.expanduser(folder))
-    data = ["[{}]".format(folder)] # title of the current directory
-    if not os.path.samefile(folder, "/"):
-      data.append("..") # option to go up a directory
-    data.append(".") # save in current folder
-    cmd = ["find", folder, "-mindepth", "1", "-type", "d"] # list all directories recursively
-    process = subprocess.Popen(cmd, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    temp = os.path.join(tempfile.gettempdir(), next(tempfile._get_candidate_names()))
+    fifo = None
     try:
-      cut = len(folder)
-      if folder[-1] != "/": # don't cut the first letter of paths after /
-        cut += 1
-      data.extend(["{}/".format(l[cut:]) for l in process.communicate()[0].splitlines()])
-    except:
-      process.kill()
-      process.wait()
-      raise
-    # show quick panel to choose folder to save in
-    self.view.window().show_quick_panel(data, functools.partial(self.chosen, folder, data), sublime.MONOSPACE_FONT, 1, functools.partial(self.previewFolder, folder, data))
+      os.mkfifo(temp)
+      fifo = os.open(temp, os.O_RDONLY | os.O_NONBLOCK)
 
-  # list the contents of a folder in the preview view via the sublime_files_preview_folder command
-  def previewFolder(self, folder, data, i):
-    if i < 1:
-      self.preview.run_command("sublime_files_preview_folder", {"folder": None})
-      return
-    folder = os.path.join(folder, data[i])
-    self.preview.run_command("sublime_files_preview_folder", {"folder": folder})
+      cmd = [
+        "kitty", "--class", "popup", "-d", "~", "bash", "-c",
+        "fd -HI -t d -E \\.git | fzf --info=hidden --preview=\"/bin/ls ~/{} -AFh --color=always\" --preview-window=right:30% --bind \"enter:accept+execute(echo {} > " + temp + ")\""
+      ]
+      subprocess.Popen(cmd).wait()
 
-  # prompt for the filename
-  def chosen(self, folder, data, i):
-    self.preview.close()
-    self.view.window().focus_view(self.view) # focus original view
-    if i < 1:
-      return
-    if data[i] == "..":
-      self.navigate(os.path.dirname(folder))
-      return
-    folder = os.path.join(folder, data[i])
-    # prompt for the filename, but allow editing the whole path
-    self.view.window().show_input_panel("Filename", folder, self.save, None, None)
+      toSave = os.read(fifo, 1024).decode()
+      if toSave:
+        toSave = os.path.join(os.path.expanduser("~"), toSave[:-1]) + "/"
+        self.view.window().show_input_panel("Save", toSave, self.save, None, None)
+    finally:
+      if fifo:
+        os.close(fifo)
+      os.remove(temp)
 
   # actually save
   def save(self, file):
@@ -133,20 +64,3 @@ class SublimeFilesSaveAsCommand(sublime_plugin.TextCommand):
       return
     self.view.retarget(file)
     self.view.run_command("save")
-
-# list the contents of a folder. This needs to be a separate command so we can use the passed `edit` to make changes to a view.
-class SublimeFilesPreviewFolderCommand(sublime_plugin.TextCommand):
-  def run(self, edit, folder):
-    if folder is None:
-      self.view.erase(edit, sublime.Region(0, self.view.size())) # just empty, dont write anything
-      return
-    cmd = ["ls", "-1AF", folder] # output directly written to preview buffer
-    process = subprocess.Popen(cmd, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    try:
-      data = process.communicate()[0]
-    except:
-      process.kill()
-      process.wait()
-      raise
-    self.view.erase(edit, sublime.Region(0, self.view.size()))
-    self.view.insert(edit, 0, data)
